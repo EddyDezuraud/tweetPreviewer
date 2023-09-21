848e547754e3c9a95f8ea83f8924d89b1f2ab143
edezuraud <edezuraud@gedicom.fr>
2023-09-22 00:33:47 +0200

    Add version number and title

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,10 @@ inject();
 </script>
 
 <template>
+  <div id="titlePage">
+    <h1 >X Post Preview</h1>
+    <span class="grey">v0.0.1</span>
+  </div>
   <Tweet/>
   <a class="twitterLink grey" href="https://twitter.com/eddydsn" target="_blank">
     @eddydsn
@@ -13,6 +17,31 @@ inject();
 
 
 <style>
+
+#titlePage {
+  position: absolute;
+  top: 20px;
+  text-align: center;
+  width: 100%;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: baseline;
+  gap: 10px;
+}
+
+#titlePage h1 {
+  margin: 0;
+  padding: 0;
+  font-size: 20px;
+  line-height: 1;
+}
+
+#titlePage > span {
+  line-height: 1;
+  font-size: 12px;
+  font-weight: 700;
+}
 
 .twitterLink {
   position: absolute;
