223603bb2209b0b5d457ea687ab8537f7be5a90f
edezuraud <edezuraud@gedicom.fr>
2023-09-22 00:27:46 +0200

    Feature random color avatar

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -9,14 +9,24 @@ const handlePaste = (event) => {
     const text = clipboardData.getData('text/plain'); // Obtenir le texte brut du clipboard
     document.execCommand('inserttext', false, text); // Insérer le texte brut sans le HTML
 };
+
+const getRandomHexColor = () => {
+    // Generate random values for red, green, and blue components
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    // Convert the values to hex and format as a color code
+    const hexColor = `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+
+    return hexColor;
+};
 </script>
 
 <template>
     <div :class="$style.wrapper">
         <div :class="$style.left">
-            <div :class="$style.avatar">
-
-            </div>
+            <div :class="$style.avatar" :style="{background: getRandomHexColor()}" ></div>
         </div>
         <div :class="$style.content">
 
@@ -50,6 +60,15 @@ const handlePaste = (event) => {
     width: 40px;
     aspect-ratio: 1;
     border-radius: 50%;
-    background: red;
+    position: relative;
+}
+
+.avatar::before {
+    position: absolute;
+    content: '';
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background: linear-gradient(45deg, rgba(255,255,255,0.04), rgba(255,255,255,0.1))
 }
 </style>
