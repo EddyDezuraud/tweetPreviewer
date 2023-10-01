87eda87ed798fd80459a3ad7d2ff97ab36c439b6
edezuraud <eddy.dezuraud@gmail.com>
2023-10-01 16:15:17 +0200

    Username form style

--- src/components/FormModal.vue
+++ src/components/FormModal.vue
@@ -2,8 +2,58 @@
 </script>
 
 <template>
-    <div>
-        you username
-        <input type="text">
+    <div :class="$style.wrapper">
+        <label :class="$style.label" for="usernameInput">What is you X username ?</label>
+        <div :class="$style.inputWrapper">
+            <div :class="$style.inputPrefix">
+                <span>@</span>
+            </div>
+            <input :class="$style.input" id="usernameInput" type="text" placeholder="username">
+        </div>
     </div>
 </template>
+
+
+<style module>
+.wrapper {
+    display: flex;
+    flex-direction: column;
+    height: calc(100vh - 40px);
+    justify-content: center;
+    align-content: flex-start;
+    width: 100%;
+    width: 800px;
+    margin: 0 auto;
+}
+
+.label {
+    text-align: left;
+    font-size: 2.4rem;
+    display: block;
+    margin-bottom: 20px;
+}
+
+.inputWrapper {
+    position: relative;
+}
+
+.inputPrefix {
+    position: absolute;
+    left: 24px;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 24px;
+}
+
+.input {
+    width: 100%;
+    padding: 16px 24px 16px 50px;
+    border-radius: 8px;
+    background: linear-gradient(180deg, rgba(255, 255, 255, .1), rgba(255, 255, 255, .08));
+    border: solid 0px rgba(255, 255, 255, .1);
+    font-size: 24px;
+    outline: none;
+}
+</style>
--- src/components/index.vue
+++ src/components/index.vue
@@ -22,52 +22,9 @@ const onValidForm = (e) => {
 </script>
 
 <template>
-  <div class="appWrapper">
+  <div>
     <Loader v-if="showLoader" @end="onEndLoader" />
     <FormModal v-else-if="showFormModal" @valid="onValidForm" />
     <Tweet v-else-if="showPreviewer" />
   </div>
 </template>
-
-
-<style>
-.appWrapper {
-  display: flex;
-  height: 100vh;
-  width: 100%;
-  justify-content: space-between;
-  flex-direction: column;
-  min-height: 100vh;
-  padding: 20px;
-  box-sizing: border-box;
-  gap: 20px;
-}
-
-#titlePage {
-  text-align: center;
-  width: 100%;
-  left: 0;
-  display: flex;
-  justify-content: center;
-  align-items: baseline;
-  gap: 10px;
-}
-
-#titlePage h1 {
-  margin: 0;
-  padding: 0;
-  font-size: 20px;
-  line-height: 1;
-}
-
-#titlePage>span {
-  line-height: 1;
-  font-size: 12px;
-  font-weight: 700;
-}
-
-.twitterLink {
-  font-weight: 800;
-  font-size: 13px;
-}
-</style>
